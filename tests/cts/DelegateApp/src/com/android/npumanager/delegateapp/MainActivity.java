76345c99c5e18eab672a5378998cc0d2138159c3
Charles Wang <charlesswang@google.com>
2025-11-25 09:10:28 -0800

    Revert^2 "Test app changes to include Model loader API in NpuManager directory."
    This reverts commit b56a28bfd73683a06ccc48fa76be8efc7b0f1845.
    
    Reason for revert: Undoing the original revert and fixing the build issue.
    
    Test: App builds and runs; fixes build error causing original revert
    Flag: TEST_ONLY
    Bug: 463669098
    Bug: 460172790
    Change-Id: Id4bf8f776dc5fdcc3e2209f059fb0b500e492654

--- tests/cts/DelegateApp/src/com/android/npumanager/delegateapp/MainActivity.java
+++ tests/cts/DelegateApp/src/com/android/npumanager/delegateapp/MainActivity.java
@@ -19,8 +19,14 @@ package com.android.npumanager.delegateapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.npumanager.ModelLoadRequest;
+import android.npumanager.ModelLoadRequestCallback;
+import android.npumanager.NpuManager;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -33,16 +39,69 @@ public class MainActivity extends Activity {
     public static final String ACTION_INFERENCE_COMPLETE = "com.android.npumanager.delegateapp."
             + "ACTION_INFERENCE_COMPLETE";
 
-
+    private NpuManager npuManager;
     private byte[] modelBuffer = null;
 
     private byte[] inputBuffer = null;
+    private Button loadModelButton;
+    private Button unloadModelButton;
+    private TextView textView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public ModelLoadRequestCallback callback =
+            new ModelLoadRequestCallback() {
+                private NpuManager.ModelLoadStatusListener mListener = null;
 
+                @Override
+                public void onCanLoadModel(
+                        ModelLoadRequest request,
+                        int status,
+                        NpuManager.ModelLoadStatusListener listener) {
+                    mListener = listener;
+
+                    if (status == NpuManager.NPU_MODEL_LOAD_STATUS_CAN_LOAD_NOW) {
+                        Log.w(TAG, "NPU_MODEL_LOAD_STATUS_CAN_LOAD_NOW - loading model now");
+                        loadModel();
+                        Log.i(
+                                TAG,
+                                "Finished loading model. Calling notifyModelLoaded() now "
+                                        + "and running NPU inference");
+                        if (mListener != null) {
+                            try {
+                                mListener.notifyModelLoaded(request);
+                            } catch (RemoteException e) {
+                                Log.e(TAG, "RemoteException created");
+                            }
+                        }
+
+                        runNpuInference();
+                    } else {
+                        Log.w(
+                                TAG,
+                                "Status is not NPU_MODEL_LOAD_STATUS_CAN_LOAD_NOW. "
+                                        + "Not loading");
+                    }
+                }
+
+                @Override
+                public void onRequestUnloadModel(ModelLoadRequest request) {
+                    unloadModel();
+                    if (mListener != null) {
+                        try {
+                            mListener.notifyModelUnloaded(request);
+                        } catch (RemoteException e) {
+                            Log.e(TAG, "RemoteException created");
+                        }
+                    }
+                }
+
+                @Override
+                public void onModelLoadRequestComplete(ModelLoadRequest request, int status) {
+                    Log.w(TAG, "Model load request complete");
+                }
+            };
+
+    public void loadModel() {
+        textView.setText("Loading Model...");
         try {
             modelBuffer = loadAssetToByteArray(getAssets(), TEST_TFLITE_MODEL);
 
@@ -52,7 +111,60 @@ public class MainActivity extends Activity {
 
         } catch (IOException e) {
             Log.e(TAG, "Failed to load model", e);
+            textView.setText("Failed to load model: " + e.toString());
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        npuManager = getApplicationContext().getSystemService(NpuManager.class);
+        loadModelButton = findViewById(R.id.load_model_btn);
+        unloadModelButton = findViewById(R.id.unload_model_btn);
+        textView = findViewById(R.id.text_view_status);
+
+        if (npuManager != null) {
+            Log.w(TAG, "NpuManager not null.");
+            textView.setText(
+                    "NpuManager is available. Use buttons below to trigger " + "callbacks");
+        } else {
+            Log.w(TAG, "NpuManager null. ");
+            textView.setText("NpuManager is null. Make sure NPU is enabled " + "for this device");
+            loadModelButton.setEnabled(false);
+            unloadModelButton.setEnabled(false);
+        }
+        ModelLoadRequest request =
+                new ModelLoadRequest.Builder(54)
+                        .setSize(NpuManager.NPU_MODEL_SIZE_LESS_THAN_1GB)
+                        .setPriority(NpuManager.NPU_MODEL_PRIORITY_NORMAL)
+                        .build();
+
+        loadModelButton.setOnClickListener(
+                v -> {
+                    try {
+                        if (npuManager != null) {
+                            Log.w(TAG, "Requesting to load the model now");
+                            textView.setText("Request loading model now");
+                            npuManager.requestLoadModel(request, callback);
+                        }
+                    } catch (RemoteException e) {
+                        Log.w(TAG, "Remote exception: " + e);
+                    }
+                });
+
+        unloadModelButton.setOnClickListener(
+                v -> {
+                    Log.i(TAG, "Unload Model button clicked.");
+                    callback.onRequestUnloadModel(request);
+                });
+    }
+
+    // Release buffers.
+    public void unloadModel() {
+        modelBuffer = null;
+        inputBuffer = null;
+        textView.setText("Model successfully unloaded");
     }
 
     public void runNpuInference() {
@@ -60,26 +172,40 @@ public class MainActivity extends Activity {
             Log.w(TAG, "Running inference....");
             long startTimeMs = System.currentTimeMillis();
             // Run in background thread to avoid blocking UI
-            new Thread(() -> {
-                final String result = runInference(modelBuffer, inputBuffer);
-                long finishedTimeMs = System.currentTimeMillis();
-                long elapsedTimeMs = finishedTimeMs - startTimeMs;
-                Log.w(TAG, "Finished running inferences. " +
-                        (getPackageName().contains("foreground") ? "foreground" : "background") +
-                        "Start Time Ms: " + startTimeMs + ", finishedTimeMs: " + finishedTimeMs +
-                        ", elapsedTimeMs: " + elapsedTimeMs + ", result " + result);
-                Intent intent = new Intent(ACTION_INFERENCE_COMPLETE);
-                intent.putExtra("timestamp", finishedTimeMs);
-                intent.putExtra("package", getPackageName());
-                Log.i(
-                        TAG,
-                        "Sending broadcast: "
-                                + intent
-                                + " with timestamp: "
-                                + intent.getLongExtra("timestamp", 0));
-                sendBroadcast(intent);
-            }).start();
+            new Thread(
+                            () -> {
+                                final String result = runInference(modelBuffer, inputBuffer);
+                                long finishedTimeMs = System.currentTimeMillis();
+                                long elapsedTimeMs = finishedTimeMs - startTimeMs;
+                                Log.w(
+                                        TAG,
+                                        "Finished running inferences. "
+                                                + (getPackageName().contains("foreground")
+                                                        ? "foreground"
+                                                        : "background")
+                                                + "Start Time Ms: "
+                                                + startTimeMs
+                                                + ", finishedTimeMs: "
+                                                + finishedTimeMs
+                                                + ", elapsedTimeMs: "
+                                                + elapsedTimeMs
+                                                + ", result "
+                                                + result);
+                                textView.setText("Finished running inference");
+                                Intent intent = new Intent(ACTION_INFERENCE_COMPLETE);
+                                intent.putExtra("timestamp", finishedTimeMs);
+                                intent.putExtra("package", getPackageName());
+                                Log.i(
+                                        TAG,
+                                        "Sending broadcast: "
+                                                + intent
+                                                + " with timestamp: "
+                                                + intent.getLongExtra("timestamp", 0));
+                                sendBroadcast(intent);
+                            })
+                    .start();
         } else {
+            textView.setText("Model not loaded");
             Log.e(TAG, "Model not loaded");
         }
     }
