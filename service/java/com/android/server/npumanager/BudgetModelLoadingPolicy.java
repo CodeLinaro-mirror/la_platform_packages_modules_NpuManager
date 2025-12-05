6f421170b35515845e9d44bad6a37705f56dfeca
John Johnson <johnrjohn@google.com>
2025-12-05 08:32:14 -0800

    Fix incorrect budget calculation
    Fix: 466203426
    Bug: 466203426
    Test: atest -c CtsNpuModelManagerTest
    Flag: com.android.npumanager.npumanager_enabled
    Change-Id: Id4f1826a8f87be9bdc324966ef5072fed29f61a1

--- service/java/com/android/server/npumanager/BudgetModelLoadingPolicy.java
+++ service/java/com/android/server/npumanager/BudgetModelLoadingPolicy.java
@@ -183,7 +183,7 @@ class BudgetModelLoadingPolicy extends NpuModelLoadingPolicy {
             mUidsToRequests.put(callingUid, uidRequests);
             if (mLoadedRequests.contains(request)) {
                 mLoadedRequests.remove(request);
-                mAvailableBudget -=
+                mAvailableBudget +=
                         mModelSizeWeights.getOrDefault(request.getSize(), Integer.MAX_VALUE);
             }
         }
