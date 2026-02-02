395ecc455b4308bcb2482365549d99ca2da3500f
Yifan Hong <elsk@google.com>
2026-02-02 14:03:08 -0800

    Let libandroid load libcom.android.npumanager lazily.
    Don't dlopen libcom.android.npumanager immediately. Instead, only lazily
    dlopen() libcom.android.npumanager on demand. This is because libandroid can
    be loaded to processes without the com.android.npumanager APEX being
    available:
    
    - In bootanimation, where /data is not yet ready
    - In art tests
    
    Bug: 466107663
    Flag: com.android.npumanager.npumanager_enabled
    Change-Id: I70d8ff2a7d19a0fffb36fcf446bbbfcee0469937

--- ndk/libandroid_shim.cpp
+++ ndk/libandroid_shim.cpp
@@ -14,89 +14,154 @@
  * limitations under the License.
  */
 
-/* libandroid_npumanager: calls into libnpumanager. */
+/* libandroid_npumanager: Let libandroid lazily load and call into libcom.android.npumanager. */
 
+#include <android-base/logging.h>
+#include <android-base/result.h>
 #include <android/npumanager/buffer.h>
 #include <android/npumanager/buffer_impl.h>
+#include <dlfcn.h>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+
+// b/479732472: Lazily load libcom.android.npumanager.so. libandroid.so might be loaded to processes
+// before the APEX is ready, so don't let libandroid.so immediately dlopen()
+// libcom.android.npumanager.so.
+namespace {
+
+using ::android::base::Error;
+using ::android::base::Result;
+
+constexpr const char kLibComAndroidNpuManager[] = "libcom.android.npumanager.so";
+
+std::string GetDlError() {
+    const char* e = dlerror();
+    return e ? e : "";
+}
+
+Result<void*> LoadLibComAndroidNpuManager() {
+    static void* library_handle = dlopen(kLibComAndroidNpuManager, RTLD_LAZY | RTLD_LOCAL);
+    static std::string error = GetDlError();
+    return library_handle ? Result<void*>(library_handle) : (Error() << error);
+}
+template <typename Fn>
+Fn LoadSymbol(const char* name) {
+    auto library_handle = LoadLibComAndroidNpuManager();
+    if (!library_handle.ok()) {
+        LOG(FATAL) << "Failed to load " << kLibComAndroidNpuManager << ": "
+                   << library_handle.error();
+        return nullptr;
+    }
+    void* symbol = dlsym(*library_handle, name);
+    if (symbol == nullptr) {
+        LOG(FATAL) << "Failed to load symbol " << name << " from " << kLibComAndroidNpuManager
+                   << ": " << dlerror();
+        return nullptr;
+    }
+    return reinterpret_cast<Fn>(symbol);
+}
+}  // namespace
+
+#define LOAD_SYMBOL(name) LoadSymbol<decltype(&name)>(#name)
 
 ANpuManager_AllocRequest* _Nonnull ANpuManager_AllocRequest_create() {
-    return ANpuManagerImpl_ANpuManager_AllocRequest_create();
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_create);
+    return symbol();
 }
 
 void ANpuManager_AllocRequest_free(ANpuManager_AllocRequest* _Nonnull request) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_free(request);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_free);
+    return symbol(request);
 }
 
 void ANpuManager_AllocRequest_setCookie(ANpuManager_AllocRequest* _Nonnull request,
                                         void* _Nullable cookieToOwn,
                                         ANpuManager_CookieDeleter _Nullable cookieDeleter) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setCookie(request, cookieToOwn, cookieDeleter);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setCookie);
+    return symbol(request, cookieToOwn, cookieDeleter);
 }
 
 void ANpuManager_AllocRequest_setDeviceNumber(ANpuManager_AllocRequest* _Nonnull request,
                                               int32_t deviceNumber) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setDeviceNumber(request, deviceNumber);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setDeviceNumber);
+    return symbol(request, deviceNumber);
 }
 
 void ANpuManager_AllocRequest_setBufferType(ANpuManager_AllocRequest* _Nonnull request,
                                             ANpuBuffer_Type bufferType) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setBufferType(request, bufferType);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setBufferType);
+    return symbol(request, bufferType);
 }
 
 void ANpuManager_AllocRequest_setSize(ANpuManager_AllocRequest* _Nonnull request, int64_t size) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setSize(request, size);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setSize);
+    return symbol(request, size);
 }
 
 void ANpuManager_AllocRequest_setBufferPriority(ANpuManager_AllocRequest* _Nonnull request,
                                                 int32_t bufferPriority) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setBufferPriority(request, bufferPriority);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setBufferPriority);
+    return symbol(request, bufferPriority);
 }
 
 void ANpuManager_AllocRequest_setFileSegmentToLoad(ANpuManager_AllocRequest* _Nonnull request,
                                                    int fdToOwn, int64_t fileOffset,
                                                    int64_t segmentLength, int64_t bufferOffset) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setFileSegmentToLoad(request, fdToOwn, fileOffset,
-                                                                  segmentLength, bufferOffset);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setFileSegmentToLoad);
+    return symbol(request, fdToOwn, fileOffset, segmentLength, bufferOffset);
 }
 
 void ANpuManager_AllocRequest_setProtectionFlags(ANpuManager_AllocRequest* _Nonnull request,
                                                  int32_t prot) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setProtectionFlags(request, prot);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setProtectionFlags);
+    return symbol(request, prot);
 }
 
 void ANpuManager_AllocRequest_setOnAlloc(ANpuManager_AllocRequest* _Nonnull request,
                                          ANpuManager_AllocCallback _Nonnull onAlloc) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setOnAlloc(request, onAlloc);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setOnAlloc);
+    return symbol(request, onAlloc);
 }
 
 void ANpuManager_AllocRequest_setOnPreempt(ANpuManager_AllocRequest* _Nonnull request,
                                            ANpuManager_PreemptCallback _Nullable onPreempt) {
-    ANpuManagerImpl_ANpuManager_AllocRequest_setOnPreempt(request, onPreempt);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_AllocRequest_setOnPreempt);
+    return symbol(request, onPreempt);
 }
 
 int ANpuManager_isSupported(ANpuManager_AllocRequest* _Nonnull const* _Nonnull requests,
                             size_t requestsLen, bool* _Nonnull outIsSupported) {
-    return ANpuManagerImpl_ANpuManager_isSupported(requests, requestsLen, outIsSupported);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_isSupported);
+    return symbol(requests, requestsLen, outIsSupported);
 }
 
 void ANpuManager_allocAsync(ANpuManager_AllocRequest* _Nonnull const* _Nonnull requests,
                             size_t requestsLen) {
-    ANpuManagerImpl_ANpuManager_allocAsync(requests, requestsLen);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuManager_allocAsync);
+    return symbol(requests, requestsLen);
 }
 
 int ANpuBuffer_free(ANpuBuffer* _Nonnull const* _Nonnull buffers, size_t buffersLen) {
-    return ANpuManagerImpl_ANpuBuffer_free(buffers, buffersLen);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuBuffer_free);
+    return symbol(buffers, buffersLen);
 }
 
 void* _Nonnull ANpuBuffer_map(ANpuBuffer* _Nonnull buf, void* _Nullable addr, size_t length,
                               int prot, int flags, off_t offset) {
-    return ANpuManagerImpl_ANpuBuffer_map(buf, addr, length, prot, flags, offset);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuBuffer_map);
+    return symbol(buf, addr, length, prot, flags, offset);
 }
 
 int ANpuBuffer_unmap(ANpuBuffer* _Nonnull buf, void* _Nonnull addr, size_t length) {
-    return ANpuManagerImpl_ANpuBuffer_unmap(buf, addr, length);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuBuffer_unmap);
+    return symbol(buf, addr, length);
 }
 
 int ANpuBuffer_setPriority(ANpuBuffer* _Nonnull buf, int32_t newBufferPriority) {
-    return ANpuManagerImpl_ANpuBuffer_setPriority(buf, newBufferPriority);
+    static auto symbol = LOAD_SYMBOL(ANpuManagerImpl_ANpuBuffer_setPriority);
+    return symbol(buf, newBufferPriority);
 }
+
+#undef LOAD_SYMBOL
